fab56fcb589f14bcfd81e9e4be3c915067db7944
StefanoMDeveloper <stenmarti02@gmail.com>
2022-03-08 16:49:14 +0100

    non funziona il faker

---
+++ database/migrations/2022_03_08_153110_create_travels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->date("data");
+            $table->string("destinazione");
+            $table->string("hotel");
+            $table->double("prezzo");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
